47f78b24564f07982292f7e3a087b7cfd3268203
jimouchen <jmc1625645320@gmail.com>
2020-09-03 21:06:03 +0800

    test string iterator

--- TestAll/testString.cpp
+++ TestAll/testString.cpp
@@ -1,10 +1,21 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main(){
-//    string s = "12312hello world...";
-    string s;
-    getline(cin, s);//输入字符串
-    cout<<s;
+int main() {
+    string s = "1523525431873";
+    for (int i = 0; i < s.length(); ++i)
+        cout << s[i];
+
+    cout << '\n';
+    for (string::iterator iter = s.begin(); iter != s.end(); iter++)
+        cout << *iter;
+
+    cout << '\n';
+    for (auto i : s)
+        cout << i;
+
+
+    cout << endl << s;
     return 0;
 }
