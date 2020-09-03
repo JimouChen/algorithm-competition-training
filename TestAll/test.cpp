3a75dfb70019cf26c9ec859ca9a218941b399418
jimouchen <jmc1625645320@gmail.com>
2020-09-03 20:31:58 +0800

    test string and other

--- TestAll/test.cpp
+++ TestAll/test.cpp
@@ -1,7 +1,46 @@
-# include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main(){
-    printf("hello");
+template<class T>
+void showMsg(vector<T> msg) {
+//    for (int j = 0; j < msg.size(); ++j)
+//        cout << msg[j] << ' ';
+//
+    auto p = msg.begin();
+    for (p; p < msg.end(); p++) {
+        cout << *p << ' ';
+    }
+    cout << '\n';
+}
+
+int main() {
+    vector<char> msg;
+    char next;
+    cin.get(next);
+    while (next != '\n') {
+        msg.push_back(next);
+        cin.get(next);
+    }
+    showMsg(msg);
+    //change value
+    msg[2] = 'e';
+    showMsg(msg);
+    //reverse
+    reverse(msg.begin(), msg.end());
+    showMsg(msg);
+    int n;
+    vector<int> In;
+    cin>>n;
+    while (n--){
+        int v;
+        cin>>v;
+        In.push_back(v);
+    }
+    reverse(In.begin(), In.end());
+    sort(In.rbegin(), In.rend());
+
+    showMsg(In);
+
     return 0;
 }
---
+++ TestAll/testString.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+//    string s = "12312hello world...";
+    string s;
+    getline(cin, s);//输入字符串
+    cout<<s;
+    return 0;
+}
