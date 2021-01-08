3e518fe04b8f90989e6218a255bbfc4b8bb9b3ef
jimouchen <jmc1625645320@gmail.com>
2021-01-09 02:15:06 +0800

    commit class basis

---
+++ course/CPPLearning/base/guard.h
@@ -0,0 +1,18 @@
+//
+// Created by JMChen on 2021/1/9.
+//
+
+#ifndef ALGORITHM_COMPETITION_TRAINING_GUARD_H
+#define ALGORITHM_COMPETITION_TRAINING_GUARD_H
+
+class Complex {
+private:
+    double re, im;
+public:
+    Complex(double r, double i) : re(r), im(i) {
+    }
+
+};
+
+
+#endif //ALGORITHM_COMPETITION_TRAINING_GUARD_H
---
+++ course/CPPLearning/base/test_guard.cpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+#include "guard.h"
+
+using namespace std;
+
+int main() {
+    return 0;
+}
