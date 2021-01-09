2eec7c17849cc276d116843b9bef7ebf5e6c5880
jimouchen <jmc1625645320@gmail.com>
2021-01-09 21:12:24 +0800

    update constructor

--- course/CPPLearning/base/guard.h
+++ course/CPPLearning/base/guard.h
@@ -12,7 +12,50 @@ public:
     Complex(double r, double i) : re(r), im(i) {
     }
 
+    Complex() {}
+
+    double real() const {
+        return re;
+    }
+
+    double imag() const {
+        return im;
+    }
+
+    double fun(const Complex &c) {
+        return c.re + c.im;
+    }
+
+    Complex &operator+=(const Complex &);//声明
 };
+
+inline Complex &Complex::operator+=(const Complex &complex) {
+    this->re += complex.re;
+    this->im += complex.im;
+    return *this;
+}
+
+inline Complex operator+(const Complex &x, const Complex &y) {
+    return Complex(x.real() + y.real(), x.imag() + y.imag());
+}
+
+inline Complex operator+(const Complex &x, const double y) {
+    return Complex(x.real() + y, x.imag());
+}
+
+inline Complex operator+(const double x, const Complex &y) {
+    return Complex(y.real() + x, y.imag());
+}
+
+inline Complex operator+(const Complex &x) {
+    return x;
+}
+
+inline Complex operator-(const Complex &x) {
+    return Complex(-x.real(), -x.imag());
+}
+
+
 
 
 #endif //ALGORITHM_COMPETITION_TRAINING_GUARD_H
--- course/CPPLearning/base/test_guard.cpp
+++ course/CPPLearning/base/test_guard.cpp
@@ -6,5 +6,16 @@
 using namespace std;
 
 int main() {
+    Complex complex(2, 1);
+    Complex complex2(2, 1);
+    Complex c1;
+    double d = 3;
+    cout << c1.fun(complex) << endl;
+    Complex c2 = complex + complex2;
+    Complex c3 = complex + d;
+    complex+=complex2;
+    cout << c2.real() << " " << c2.imag()<<"\n";
+    cout << c3.real() << " " << c3.imag()<<"\n";
+    cout << complex.real() << " " << complex.imag()<<"\n";
     return 0;
 }
