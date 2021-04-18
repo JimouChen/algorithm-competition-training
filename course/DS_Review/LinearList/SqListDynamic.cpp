557908dff2a01672a2e25a52c3ec7d6ad9bbaeea
JimouChen <jmc1625645320@gmail.com>
2021-04-18 22:00:46 +0800

    commit review ds

---
+++ course/DS_Review/LinearList/SqListDynamic.cpp
@@ -0,0 +1,18 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+#define maxsize 50
+/** 动态顺序表*/
+typedef int ElemType;
+typedef struct {
+    ElemType data[maxsize];
+    int length;
+} SqList;
+
+void initList(SqList &L) {
+    L.length = 0;
+}
+
+int main() {
+    return 0;
+}
---
+++ course/DS_Review/LinearList/SqListStatic.cpp
@@ -0,0 +1,19 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+#define maxsize 50
+/** 静态顺序表*/
+typedef int ElemType;
+typedef struct {
+    ElemType data[maxsize];
+    int length;
+} SqList;
+
+void initList(SqList &L) {
+    L.length = 0;
+}
+
+int main() {
+    SqList l;
+    return 0;
+}
