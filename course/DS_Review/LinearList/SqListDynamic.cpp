227d57f213365c006209c30f0d23a8ce3746456e
JimouChen <jmc1625645320@gmail.com>
2021-04-18 23:49:03 +0800

    update

--- course/DS_Review/LinearList/SqListDynamic.cpp
+++ course/DS_Review/LinearList/SqListDynamic.cpp
@@ -1,16 +1,29 @@
 # include <bits/stdc++.h>
 
+# define initLength 20
 using namespace std;
-#define maxsize 50
 /** 动态顺序表*/
 typedef int ElemType;
 typedef struct {
-    ElemType data[maxsize];
+    ElemType *data;
+    int maxsize;
     int length;
-} SqList;
+} SeqList;
 
-void initList(SqList &L) {
+void initList(SeqList &L) {
+    L.data = (ElemType *) malloc(sizeof(ElemType) * initLength);
     L.length = 0;
+    L.maxsize = initLength;
+}
+
+void increaseLength(SeqList &L, int len) {
+    ElemType *p = L.data;//为了把原来的元素移动到新的空间
+    L.maxsize += len;
+    L.data = (ElemType *) malloc(sizeof(ElemType) * L.maxsize);
+    for (int i = 0; i < L.length; i++) {
+        L.data[i] = p[i];
+    }
+    free(p);
 }
 
 int main() {
--- course/DS_Review/LinearList/SqListStatic.cpp
+++ course/DS_Review/LinearList/SqListStatic.cpp
@@ -13,7 +13,32 @@ void initList(SqList &L) {
     L.length = 0;
 }
 
+//在第i(0开始)个位置插入,这里根据题意来就行
+bool insert(SqList &L, int i, int e) {
+    if (i < 0 || i > L.length || L.length >= maxsize)
+        return false;
+    for (int j = L.length; j > i; j--) {
+        L.data[j] = L.data[j - 1];
+    }
+    L.data[i] = e;
+    L.length++;
+    return true;
+}
+
+void display(SqList L) {
+    for (int i = 0; i < L.length; i++)
+        cout << L.data[i] << " ";
+    cout << endl;
+}
+
 int main() {
     SqList l;
+    initList(l);
+    for (int i = 1; i <= 10; i++) {
+        insert(l, i - 1, i);
+    }
+    display(l);
+    insert(l, 4, 666);
+    display(l);
     return 0;
 }
